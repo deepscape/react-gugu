9fe97f859914dfb30972ab9b909d997178a419d7
deepscape <hdkim.scala@gmail.com>
2024-05-29 09:42:48 +0900

    React-Router 중첩 라우팅 적용 : p.62

--- frontend/mall/src/layouts/BasicLayout.jsx
+++ frontend/mall/src/layouts/BasicLayout.jsx
@@ -3,12 +3,18 @@ import BasicMenu from "../components/menu/BasicMenu"
 const BasicLayout = (props) => {
   return (
     <>
+
+      {/* 기존 헤더 대신 BasicMenu */}
       <BasicMenu></BasicMenu>
-      <div className="bg-white my-5 w-full flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
-        <main className="bg-sky-300 md:w-2/3 lg:w-3/4 px-5 py-40">
+
+      {/* 상단 여백 my-5 제거 */}
+      <div className="bg-white w-full flex flex-col my-1 space-y-1 md:flex-row md:space-x-1 md:space-y-0">
+        <main className="bg-sky-300 md:w-4/5 lg:w-3/4 px-5 py-5">
           {props.children}
         </main>
-        <aside className="bg-green-300 md:w-1/3 lg:w-1/4 px-5 py-40">
+
+        {/* 상단 여백 py-40 제거 flex 제거 */}
+        <aside className="bg-green-300 md:w-1/5 lg:w-1/4 px-5 py-5">
           <h1 className="text-2xl md:text-4xl">Sidebar</h1>
         </aside>
       </div>
--- frontend/mall/src/router/root.js
+++ frontend/mall/src/router/root.js
@@ -6,6 +6,8 @@ import { createBrowserRouter } from "react-router-dom";
 const Loading = <div>Loading...</div>;
 const Main = lazy(() => import("../pages/MainPage"));
 const About = lazy(() => import("../pages/AboutPage"));
+const TodoIndex = lazy(() => import("../pages/todo/IndexPage"));
+const TodoList = lazy(() => import("../pages/todo/ListPage"));
 
 const root = createBrowserRouter([
   {
@@ -23,6 +25,24 @@ const root = createBrowserRouter([
         <About />
       </Suspense>
     ),
+  },
+  {
+    path: "todo",
+    element: (
+      <Suspense fallback={Loading}>
+        <TodoIndex />
+      </Suspense>
+    ),
+    children: [
+      {
+        path: "list",
+        element: (
+          <Suspense fallback={Loading}>
+            <TodoList />
+          </Suspense>
+        ),
+      },
+    ],
   },
 ]);
 
