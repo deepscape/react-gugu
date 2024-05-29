9102b2ea7c41e916ce180778ddeab6db286f99ea
deepscape <hdkim.scala@gmail.com>
2024-05-29 16:22:03 +0900

    중첩 라우팅 분리와 Redirection : p.65

--- frontend/mall/src/router/root.jsx
+++ frontend/mall/src/router/root.jsx
@@ -1,13 +1,13 @@
 import { Suspense, lazy } from "react";
 
 import { createBrowserRouter } from "react-router-dom";
+import todoRouter from "./todoRouter";
 // const { createBrowserRouter } = require("react-router-dom");
 
 const Loading = <div>Loading...</div>;
 const Main = lazy(() => import("../pages/MainPage"));
 const About = lazy(() => import("../pages/AboutPage"));
 const TodoIndex = lazy(() => import("../pages/todo/IndexPage"));
-const TodoList = lazy(() => import("../pages/todo/ListPage"));
 
 const root = createBrowserRouter([
   {
@@ -33,16 +33,7 @@ const root = createBrowserRouter([
         <TodoIndex />
       </Suspense>
     ),
-    children: [
-      {
-        path: "list",
-        element: (
-          <Suspense fallback={Loading}>
-            <TodoList />
-          </Suspense>
-        ),
-      },
-    ],
+    children: todoRouter(),
   },
 ]);
 
