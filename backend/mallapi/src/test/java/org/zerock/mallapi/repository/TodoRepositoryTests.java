edd4bfa7de83caee492c7f16a9a7e7c969ee476c
deepscape <hdkim.scala@gmail.com>
2024-06-03 18:40:02 +0900

    JPA 데이터 추가 테스트 : p.92

--- backend/mallapi/src/test/java/org/zerock/mallapi/repository/TodoRepositoryTests.java
+++ backend/mallapi/src/test/java/org/zerock/mallapi/repository/TodoRepositoryTests.java
@@ -4,6 +4,10 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.mallapi.domain.Todo;
+
+import java.time.LocalDate;
+import java.util.stream.IntStream;
 
 @SpringBootTest
 @Log4j2
@@ -16,5 +20,16 @@ public class TodoRepositoryTests {
     public void test1() {
         log.info("-------------------------------");
         log.info(todoRepository);
+    }
+
+    @Test
+    public void testInsert() {
+        IntStream.rangeClosed(0, 99)
+                .mapToObj(i -> Todo.builder()
+                        .title("Title..." + i)
+                        .dueDate(LocalDate.of(2023, 12, 31))
+                        .writer("user00")
+                        .build())
+                .forEach(todoRepository::save);
     }
 }
