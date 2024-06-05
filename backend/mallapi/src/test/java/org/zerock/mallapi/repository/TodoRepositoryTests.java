786e55831a5bbba1fa970a696a7607b098978593
deepscape <hdkim.scala@gmail.com>
2024-06-05 14:14:25 +0900

    Paging 처리 테스트 : p.98

--- backend/mallapi/src/test/java/org/zerock/mallapi/repository/TodoRepositoryTests.java
+++ backend/mallapi/src/test/java/org/zerock/mallapi/repository/TodoRepositoryTests.java
@@ -4,6 +4,10 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.mallapi.domain.Todo;
 
 import java.time.LocalDate;
@@ -62,4 +66,14 @@ public class TodoRepositoryTests {
 
         todoRepository.deleteById(tno);
     }
+
+    @Test
+    public void testPaging() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("tno").descending());
+        Page<Todo> result = todoRepository.findAll(pageable);
+        log.info(result.getTotalElements());
+
+        result.getContent().forEach(todo -> log.info(todo));
+    }
+
 }
