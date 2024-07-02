763286ec6e694815a0dac4b2bb3ae096c3399982
deepscape <hdkim.scala@gmail.com>
2024-07-02 17:31:49 +0900

    파일 저장 Controller 작성 및 PostMan 테스트 : p.189

---
+++ backend/mallapi/src/main/java/org/zerock/mallapi/controller/ProductController.java
@@ -0,0 +1,35 @@
+package org.zerock.mallapi.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.zerock.mallapi.dto.ProductDTO;
+import org.zerock.mallapi.util.CustomFileUtil;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@Log4j2
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final CustomFileUtil fileUtil;
+
+    @PostMapping("/")
+    public Map<String, String> register(ProductDTO productDTO) {
+        log.info("register product : {}", productDTO);
+
+        List<MultipartFile> files = productDTO.getFiles();
+        List<String> uploadFileNames = fileUtil.saveFiles(files);
+
+        productDTO.setUploadFileNames(uploadFileNames);
+        log.info(uploadFileNames);
+
+        return Map.of("RESULT", "SUCCESS");
+    }
+}
